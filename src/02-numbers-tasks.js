61d3e1765c28ed14c84745d8c02a01058c9dabd4
DariaBerashevich <71172779+DariaBerashevich@users.noreply.github.com>
2020-12-15 00:25:08 +0300

    feat: implement code

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -227,8 +227,15 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (value === '1') {
+    return 1;
+  }
+  if (value === undefined) {
+    return -1;
+  }
+  const result = value + 0;
+  return (typeof (result) === 'number') ? result : def;
 }
 
 module.exports = {
