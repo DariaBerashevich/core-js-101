7ef1290ce4e21c44f846e767fe6b2327d88d337d
DariaBerashevich <71172779+DariaBerashevich@users.noreply.github.com>
2020-12-15 20:20:03 +0300

    feat: implement code

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -356,8 +356,9 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const result = arr.filter((elem) => Boolean(elem) === false);
+  return result.length;
 }
 
 /**
